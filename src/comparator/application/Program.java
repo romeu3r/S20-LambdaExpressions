41a9ded7b2f79a5e8db6863e1ad9b8462c0a84fe
romeu3r <romeu3r@gmail.com>
2022-02-14 19:21:39 -0300

    Before changes.

---
+++ src/comparator/application/Program.java
@@ -0,0 +1,21 @@
+package comparator.application;
+
+import comparator.entities.Product;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        Collections.sort(list);
+
+        list.forEach(System.out::println);
+    }
+}
