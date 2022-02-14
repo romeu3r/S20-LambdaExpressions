2695ea372528722d0c3471aa8154acadc214e2be
romeu3r <romeu3r@gmail.com>
2022-02-14 20:17:26 -0300

    Predicate Example 4. (Lambda declared)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,10 @@
 package application;
 
 import entities.Product;
-import util.ProductPredicate;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class Program {
     public static void main(String[] args) {
@@ -14,7 +14,8 @@ public class Program {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        list.removeIf(Product::nonStaticProductPredicate);
+        Predicate<Product> pred = p -> p.getPrice() >= 1000;
+        list.removeIf(pred);
 
         list.forEach(System.out::println);
     }
