09970dd1a5cc4fa90068afd10e11b24cc023347a
romeu3r <romeu3r@gmail.com>
2022-02-14 19:34:48 -0300

    First Lambda implementation V3.

--- src/comparator/application/Program.java
+++ src/comparator/application/Program.java
@@ -3,7 +3,6 @@ package comparator.application;
 import comparator.entities.Product;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class Program {
@@ -14,9 +13,7 @@ public class Program {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        Comparator<Product> comp = (o1, o2) -> o1.getName().toUpperCase().compareTo(o2.getName().toUpperCase());
-
-        list.sort(comp);
+        list.sort((o1, o2) -> o1.getName().toUpperCase().compareTo(o2.getName().toUpperCase()));
 
         list.forEach(System.out::println);
     }
