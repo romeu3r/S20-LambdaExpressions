1c519e2c922d03a1a386a8f62372abdb03810faa
romeu3r <romeu3r@gmail.com>
2022-02-14 20:15:47 -0300

    Predicate Example 3. (No static method.)

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,7 +14,7 @@ public class Program {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        list.removeIf(Product::staticProductPredicate);
+        list.removeIf(Product::nonStaticProductPredicate);
 
         list.forEach(System.out::println);
     }
--- src/entities/Product.java
+++ src/entities/Product.java
@@ -32,6 +32,10 @@ public class Product {
         return product.getPrice() >= 1000;
     }
 
+    public boolean nonStaticProductPredicate() {
+        return price >= 1000;
+    }
+
     @Override
     public String toString() {
         return "Product{" + "name='" + name + '\'' + ", price=" + price + '}';
