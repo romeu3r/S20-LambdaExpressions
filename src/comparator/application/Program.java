9acc7aa66767a45d04ed186e60745ae1796559e1
romeu3r <romeu3r@gmail.com>
2022-02-14 19:29:37 -0300

    Comparator with anonymous Class.

--- src/comparator/application/Program.java
+++ src/comparator/application/Program.java
@@ -3,6 +3,7 @@ package comparator.application;
 import comparator.entities.Product;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Program {
@@ -13,7 +14,14 @@ public class Program {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
-        list.sort(new MyComparator());
+        Comparator<Product> comp = new Comparator<Product>() {
+            @Override
+            public int compare(Product o1, Product o2) {
+                return o1.getName().toUpperCase().compareTo(o2.getName().toUpperCase());
+            }
+        };
+
+        list.sort(comp);
 
         list.forEach(System.out::println);
     }
